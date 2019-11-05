93c5c3802523910aa6c23bc249a61e3ae9a9e165
Kieron Browne <kbrowne@pivotal.io>
2019-11-05 14:06:40 +0000

    pushd
    Co-authored-by: Julia Nedialkova <julianedialkova@hotmail.com>

--- ci/create-and-upload-release.sh
+++ ci/create-and-upload-release.sh
@@ -5,7 +5,7 @@ export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=$PASSWORD
 export BOSH_CA_CERT=$CA_CERT
 
-push test-log-emitter-release
+pushd test-log-emitter-release
   bosh create-release --force
   bosh clean-up
   bosh -n -e $TARGET upload-release
